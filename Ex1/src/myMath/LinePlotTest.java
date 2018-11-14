1120dff4cb37254fa6707a67562e21411c9923ad
mgarusi101@gmail.com <Aknvvnkl1>
2018-11-14 19:31:51 +0200

    I added remarks to the plot function, and changed the input to x,x1.

--- Ex1/src/myMath/LinePlotTest.java
+++ Ex1/src/myMath/LinePlotTest.java
@@ -12,31 +12,32 @@ import de.erichseifert.gral.plots.lines.LineRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 
 public class LinePlotTest extends JFrame {
-    public LinePlotTest(Polynom p,double X,double Y) {
+    public LinePlotTest(Polynom p,double x,double x1) {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800, 600);
         // Insert rest of the code here
         
-        DataTable data = new DataTable(Double.class, Double.class);
-        DataTable maxmin = new DataTable(Double.class, Double.class);
+        DataTable data = new DataTable(Double.class, Double.class); // contains the points to build the graph
+        DataTable maxmin = new DataTable(Double.class, Double.class); // contains the min max points
 
-        for (double x = X; x <=Y; x+=0.001) {
-            double y = p.f(x);
-            if (p.derivative().f(x)<0.01&& p.derivative().f(x)>-0.01) {
+          // loop that runs over the function points at range, and saves them 
+        for (; x <=x1; x+=0.001) { 
+            double y = p.f(x);		// saves y value using the f function of polynom
+            if (p.derivative().f(x)<0.01&& p.derivative().f(x)>-0.01) {  // calculates and saves the min max points
             	maxmin.add(x,y);}
-            data.add(x, y);
+            data.add(x, y); // add the polynom points to the collection
         }
         
-        XYPlot points = new XYPlot(maxmin);	
+        XYPlot points = new XYPlot(maxmin);	//plot of min max points
         getContentPane().add(new InteractivePanel(points));
     
-        XYPlot plot = new XYPlot(data,maxmin);
+        XYPlot plot = new XYPlot(data,maxmin);// common plot of the polynom graph, and the points
         getContentPane().add(new InteractivePanel(plot));   
         
-        LineRenderer lines = new DefaultLineRenderer2D();
+        LineRenderer lines = new DefaultLineRenderer2D(); // using the line function to create a graph from the ' data' points
         plot.setLineRenderers(data, lines);
         
-        Color color = new Color(0.0f, 0.8f, 0.1f); 
+       Color color = new Color(0.0f, 0.8f, 0.1f);  //sets the colors, polynom graph - green, point - black
         plot.getPointRenderers(data).get(0).setColor(color);
         plot.getLineRenderers(data).get(0).setColor(color);
         
